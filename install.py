9f6ff96529b976ad8ec70a64eaabafcf27577299
scrapyomama <scrapyomama@gmail.com>
2023-02-09 18:02:25 +0100

    sym

--- install.py
+++ install.py
@@ -3,9 +3,10 @@ import subprocess
 import os
 
 # implement pip as a subprocess:
-subprocess.check_call([sys.executable, '-m', 'pip', 'install', '-r' 'requirements.txt'])
-os.system('''echo 'alias sym="python3 $HOME/pySYM/cli.py"' >> ~/.bash_profile''')
-os.system('''echo 'alias sym="python3 $HOME/pySYM/cli.py"' >> ~/.zshrc''')
+def install():
+    subprocess.check_call([sys.executable, '-m', 'pip', 'install', '-r' 'requirements.txt'])
+    os.system('''echo 'alias sym="python3 $HOME/pySYM/cli.py"' >> ~/.bash_profile''')
+    os.system('''echo 'alias sym="python3 $HOME/pySYM/cli.py"' >> ~/.zshrc''')
 
 
 
