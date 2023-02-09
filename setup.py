729db20bfc082ed7fb3ce82e92447918cd1313f5
scrapyomama <scrapyomama@gmail.com>
2023-02-09 18:16:52 +0100

    sym

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="symTest",
+    name="sym",
     version="0.0.1",
     author="Thomas Garcia",
     author_email="garciathomas@gmail.com",
     description="Scrap Yo Mama",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/mrsoyer/symtest",
+    url="https://github.com/mrsoyer/pySYM",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
