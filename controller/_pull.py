aa39f6eade896eaca50b00294f6f06ef4b316a4d
scrapyomama <scrapyomama@gmail.com>
2023-10-05 16:27:09 +0200

    m

---
+++ controller/_pull.py
@@ -0,0 +1,19 @@
+import os
+
+
+
+def run(request):
+    current_script_path = os.path.abspath(__file__)
+    current_script_directory = os.path.dirname(current_script_path)
+    env_path = os.path.join(current_script_directory,'..', '.env')
+    env = ""
+    with open(env_path) as f:
+        lines = f.readlines()
+        #print(lines)
+        for line in lines:
+            #print(line)
+            env = env+str(line).replace("\n",",")
+        env = env[:-1]
+        #print('gcloud functions deploy sym --trigger-http --runtime python311 --allow-unauthenticated --region=europe-west1 --entry-point=http --set-env-vars '+env+' --source  https://source.developers.google.com/projects/xtra-mobile-app/repos/github_mrsoyer_pysym/moveable-aliases/main/paths/.')
+        os.system('git pull')
+        
