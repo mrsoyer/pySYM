6008ce0f0b0624bdf49d6580b1c3589a82009448
Marwan Rouissi <marwan.rouissi@laplateforme.io>
2023-10-09 14:09:46 +0200

    m

--- controller/peMethods.py
+++ controller/peMethods.py
@@ -1,16 +1,16 @@
 
 
-def scrape_offers(request, SYM):
+def scrapeOffers(request, SYM):
     # url = "https://candidat.pole-emploi.fr/offres/recherche?domaine=M,D,G,H,M16,K,N&emission=1&lieux=13201&natureOffre=FS,E2&offresPartenaires=false&qualification=0,X&rayon=50&tri=1&typeContrat=CDI,CDD,SAI"
     # return request["get"]["url"]
     return SYM.app('pe').scrape_job_offers(request["body"]["url"])
 
-def scrape_details(request, SYM):
+def scrapeDetails(request, SYM):
     # ref = "162XCHZ"
     # return request["get"]["ref"]
     return SYM.app('pe').scrape_job_details(request["get"]["ref"])
 
-def get_company_info(request, SYM):
+def getCompanyInfo(request, SYM):
     # company_name = "Di Micheli Ristorante"
     # city = "Aix-en-Provence"
     # return request["get"]["company_name"]
