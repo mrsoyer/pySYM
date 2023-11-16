7dc642e0f1dce876a8deedec3597b98f177d04b8
Marwan Rouissi <marwan.rouissi@laplateforme.io>
2023-11-16 10:01:36 +0100

    m

--- controller/pe_functions.py
+++ controller/pe_functions.py
@@ -187,6 +187,8 @@ def update_company_v2(request, SYM):
                 company_id = company[0]
                 company_details["company_id"] = company_id
                 res.append(company_details)
+            else:
+                SYM.app('postgre').update_pe_company(company[0], "not available", "not available", "not available", "not available", "not available")
             
             # try:
             #     SYM.app('postgre').update_pe_company(company[0], company_details["address"], company_details["website"], company_details["phone"], company_details["lat"], company_details["lng"])
